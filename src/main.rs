1cd278e965328dcc62be39ec20ae78f8ae2f158d
Peter Stanko <peter.stanko0@gmail.com>
2022-06-12 21:41:34 +0200

    Add structopt to parse CLI options

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
 use anyhow::Ok;
 use remotex::domain::settings::AppSettings;
+use structopt::StructOpt;
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
+    let args = CliApp::from_args();
+
     let cfg = AppSettings::load_default_config()?;
 
     println!("Loaded settings: {:?}", cfg);
 
-    remotex::web::server::serve_web_server(cfg.clone()).await?;
+    match args.sub {
+        SubCmd::Serve(_serve) => {
+            remotex::web::server::serve_web_server(cfg.clone()).await?;
+        }
+    };
 
     Ok(())
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "remotex")]
+struct CliApp {
+    #[structopt(subcommand)]
+    pub sub: SubCmd,
+}
+
+#[derive(Debug, StructOpt)]
+pub enum SubCmd {
+    #[structopt(name = "serve")]
+    Serve(Serve),
+}
+
+#[derive(Debug, StructOpt)]
+pub struct Serve {
+    pub bar: Option<String>,
 }
